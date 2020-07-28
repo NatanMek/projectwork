90e8719866621d12fce7e713527c3d3f3d55fcdd
Natan Mekonnen <mekonnen93@gmail.com>
2020-07-28 15:15:44 +0200

    commit

--- R&D-Analisi-Terreno/Bio_Sensore.py
+++ R&D-Analisi-Terreno/Bio_Sensore.py
@@ -1,9 +1,8 @@
-import sqlite3
-import random
 import datetime
-import matplotlib.pyplot as plt
+import random
+import sqlite3
 import time
-
+import plotly.graph_objects as go
 
 # Variables
 nome = 'Sensore-Biologico'
@@ -25,7 +24,7 @@ def start():
             cur.execute('INSERT INTO Dati_Sensore (Nome,Umidita,Carica_Batterica,Temperatura,Log) \
                 VALUES (?, ?, ?, ?, ?)', (nome, str(umidita) + '%', str(carica_batterica) + '%', str(temperatura) + u"\N{DEGREE SIGN}", str(log)))
             print(
-                f"Scan completed. This is the data inserted into DB:\n Nome: {nome}\n Temperatura: {temperatura}\N{DEGREE SIGN}\n Umidità: {umidita}%\n Carica Batterica: {carica_batterica}%")
+                f"Scan completed. This is the data inserted into DB:\n Nome: {nome}\n Temperatura: {temperatura}\N{DEGREE SIGN}\n Umidità: {umidita}%\n Carica Batterica: {carica_batterica}%\n Data: {log}")
         except sqlite3.Error as error:
             print("Failed to insert data into sqlite table", error)
 
@@ -33,30 +32,31 @@ def start():
             cur.close()
 
 
-def analyse_data():
+def fetch_data():
 
     with db:
         cur = db.cursor()
         try:
-            for row in cur.execute('SELECT * FROM Dati_Sensore WHERE Id_Sensore = 1'):
-                print(f'\n Nome: {row[1]}\n Umidità: {row[2]}\n Carica Batterica: {row[3]}\n Temperatura: {row[4]}')
+            cur.execute('SELECT * FROM Dati_Sensore')
+            result = cur.fetchall()
+            for row in result:
+                print(f'\n Nome: {row[1]}\n Umidità: {row[2]}\n Carica Batterica: {row[3]}\n Temperatura: {row[4]}\n Data: {row[5]}')
         except sqlite3.Error as error:
             print("Failed to insert data into sqlite table", error)
 
         finally:
             cur.close()
 
-    # labels = ['Umidità', 'Carica Batterica']
-    # quantity = [umidita, carica_batterica]
-    #
-    # colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
-    #
-    # plt.pie(quantity, labels=labels, colors=colors, autopct='%1.1f%%',
-    #         shadow=True, startangle=90)
-    #
-    # plt.axis('equal')
-    #
-    # plt.show()
+    cities = ['Milano', 'Torino', 'Roma', 'Napoli', 'Venezia']
+
+    fig = go.Figure(data=[
+        go.Bar(name='Umidità', x=cities, y=[result[0][2], result[1][2], result[2][2], result[3][2], result[4][2]]),
+        go.Bar(name='Carica Batterica', x=cities, y=[result[0][3], result[1][3], result[2][3], result[3][3], result[4][3]]),
+        go.Bar(name='Temperatura', x=cities, y=[result[0][4], result[1][4], result[2][4], result[3][4], result[4][4]])
+    ])
+    # Change the bar mode
+    fig.update_layout(barmode='group')
+    fig.show()
 
 
 def updateStart(total, progress):
@@ -113,7 +113,7 @@ def main():
                     time.sleep(.1)
                     updateFetchData(runs, run_num + 1)
                 print("Fetching Completed!")
-                analyse_data()
+                fetch_data()
                 break
             else:
                 print("Invalid code entered, please try again... ")
