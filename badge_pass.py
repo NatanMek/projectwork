ab761c269c00f66ea0ca4c5c0ebabca1364463cc
Natan Mekonnen <mekonnen93@gmail.com>
2020-09-15 13:09:28 +0200

    commit

--- badge_pass.py
+++ badge_pass.py
@@ -1,65 +1,176 @@
-import random
+import sqlite3
 import datetime
+
+conn = sqlite3.connect('tornelli.db')
+
 date_format = "%d/%m/%Y %H:%M:%S"
-
-Name = []
-Surname = []
-Tax_code = []
-Badge_number = []
-Presence = []
+data = datetime.datetime.now().strftime(date_format)
 
 
-def newbadge():
+def setUser(_nome, _cognome, _cf, _indirizzo):
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO [Utente](Nome,Cognome,CF,Indirizzo) VALUES (?,?,?,?)",
+                  (_nome, _cognome, _cf, _indirizzo))
 
-    name = input("Nome: ")
-    Name.append(name)
-    surname = input("Surname: ")
-    Surname.append(surname)
-    tax_code = input("C.F: ")
-    Tax_code.append(tax_code)
-    Presence.append(False)
-    date = datetime.datetime.now().strftime(date_format)
-    Badge_number.append(random.randint(1, 100))
-    print(f'This is your data:\n Name:{Name[-1]}\n Surname:{Surname[-1]}\n Tax Code:{Tax_code[-1]}\n Badge_number:{Badge_number[-1]}')
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite tab", error)
+    finally:
+        c.close()
+
+
+def setUserRole(r):
+    c = conn.cursor()
+    try:
+        c.execute(
+            "INSERT INTO [RuoloUtente](Ruolo,Utente) VALUES ((SELECT IDruolo FROM [Ruolo] WHERE Nome = ?),(SELECT max (IDutenteBadge) FROM [Utente]))",
+            [r])
+
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite tab", error)
+    finally:
+        c.close()
+
+
+def newBadge():
+    nome = input('Nome: ')
+    cognome = input('Cognome: ')
+
+    while True:
+        cf = input('Codice Fiscale: ')
+        with conn:
+            c = conn.cursor()
+            try:
+                c.execute("SELECT CF FROM [Utente]")
+                check = c.fetchall()
+
+            except sqlite3.Error as error:
+                print("Failed to insert data into sqlite table", error)
+
+            finally:
+                c.close()
+        cont = 0
+        for i in range(len(check)):
+            if cf == check[i][0]:
+                cont = cont + 1
+        if cont == 1:
+            print("Codice fiscale già presente!")
+        else:
+            break
+
+    ind = input('Indirizzo: ')
+
+    while True:
+        ruolo = input("Ruolo: ")
+        with conn:
+            cursore = conn.cursor()
+            try:
+                cursore.execute("SELECT Nome FROM [Ruolo]")
+                ctrl = cursore.fetchall()
+
+            except sqlite3.Error as error:
+                print("Failed to insert data into sqlite table", error)
+
+            finally:
+                cursore.close()
+        cont = 0
+        for i in range(len(ctrl)):
+            if ruolo == ctrl[i][0]:
+                cont = cont + 1
+        if cont == 1:
+            break
+        else:
+            print(f"Immettere un Ruolo esistente! {ctrl}")
+
+    setUser(nome, cognome, cf, ind)
+    setUserRole(ruolo)
+    with conn:
+        c = conn.cursor()
+        try:
+            c.execute("SELECT max (IDutenteBadge) FROM [Utente]")
+            ck = c.fetchone()
+
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table", error)
+        finally:
+            c.close()
+
+        print('Operazione andata a buon fine! Il suo numero badge è:', ck)
 
 
 def access():
+    while True:
+        room = str(input('In quale stanza desidera accedere?'))
+        with conn:
+            cursore = conn.cursor()
+            try:
+                cursore.execute("SELECT Nome FROM [Stanza]")
+                righe = cursore.fetchall()
 
-    number = int(input("Insert your Badge number: "))
-    cont = 0
-    for i in range(len(Badge_number)):
-        if number == Badge_number[i]:
-            cont = cont + 1
-            Presence[i] = True
-    if cont > 0:
-        print(f"Welcome {Name[-1]}!")
-    else:
-        print("The Badge number you've inserted is incorrect")
+            except sqlite3.Error as error:
+                print("Failed to insert data into sqlite table", error)
 
-
-def exit():
-
-    number = int(input("Insert your Badge number: "))
-    cont = 0
-    for i in range(len(Badge_number)):
-        if number == Badge_number[i]:
-            cont = cont + 1
-            Presence[i] = False
-    if cont > 0:
-        print(f"Goodbye {Name[-1]}!")
-    else:
-        print("The Badge number you've inserted is incorrect")
-
-
-while True:
-
-    print("1: Access\n2: Exit\n3: Create new User")
-    choice = int(input("> "))
-    if 0 < choice < 4:
-        if choice == 1:
-            access()
-        if choice == 2:
-            exit()
+            finally:
+                cursore.close()
+        cont = 0
+        for i in range(len(righe)):
+            if room == righe[i][0]:
+                cont = cont + 1
+        if cont == 1:
             break
-        if choice == 3:
-            newbadge()
+        else:
+            print(f"Immettere una stanza esistente! {righe}")
+
+    while True:
+        numero = int(input('Inserire numero Badge: '))
+        with conn:
+            c = conn.cursor()
+            try:
+                c.execute("SELECT IDutenteBadge FROM [Utente]")
+                riga = c.fetchall()
+                c.execute("SELECT Cognome FROM Utente WHERE IDutenteBadge = ?", (numero,))
+                d = c.fetchone()
+            except sqlite3.Error as error:
+                print("Failed to insert data into sqlite table", error)
+            finally:
+                c.close()
+        cont = 0
+        for i in range(len(riga)):
+            if numero == riga[i][0]:
+                cont = cont + 1
+        if cont >= 1:
+            break
+        else:
+            print("Numero badge inesistente")
+    with conn:
+        c = conn.cursor()
+        try:
+            c.execute(
+                "SELECT Ruolo.Nome FROM Ruolo join RuoloUtente on Ruolo.IDruolo=RuoloUtente.Ruolo WHERE RuoloUtente.Utente = ?",
+                (numero,))
+            r = c.fetchone()
+            c.execute(
+                "INSERT INTO [Accesso](Stanza,Utente,data_ora) VALUES ((SELECT IDstanza FROM Stanza WHERE Nome=?),(?),(?))",
+                (room, numero, data,))
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table", error)
+        finally:
+            c.close()
+
+    print("Benvenuto/a:", d, 'Ruolo:', r)
+    print("Accesso effettuato il:", data)
+
+
+def main():
+    while True:
+        print(
+            ' \n ( 1 ) Entra nel Tornello\n ( 2 ) Crea Nuovo Badge\n')
+        scelta = int(input("-> "))
+        if scelta == 1:
+            access()
+
+        if scelta == 2:
+            newBadge()
+
+
+main()
